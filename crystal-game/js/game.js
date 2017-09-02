0f1250e7ddc0faee803223c6e8213c89c5332406
Margo Y <margoyearwood@gmail.com>
2017-09-02 08:43:35 -0400

    Add files via upload

---
+++ crystal-game/js/game.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Elements
+    var buttons = $('.crystal-button');
+    var winsCountDOM = $('#wins-count');
+    var lossesCountDOM = $('#losses-count');
+    var scoreDOM = $('#score');
+    var numberDOM = $('#number-container');
+    // Vars
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+    var randomNum = 0;
+    var crystalValues = [];
+
+    function init() {
+        buttons.click(handleCrystalPressed);
+        setupGame();
+        lossesCountDOM.html(0);
+        winsCountDOM.html(0);
+    }
+
+    function handleCrystalPressed(e) {
+        var crystalNum = e.target.value;
+        console.log(e);
+        console.log('value: ', crystalNum);
+        updateScore(crystalNum);
+    }
+
+    function setupGame() {
+        setupButtons();
+        randomNum = generateRandomNum(19, 120);
+        score = 0;
+        scoreDOM.html(score);
+        numberDOM[0].innerHTML = randomNum;
+    }
+
+    function setupButtons() {
+        for (var i = 0; i < buttons.length; i++) {
+            var buttonVal = generateCrystalNum();
+            $(buttons[i]).val(buttonVal);
+        }
+    }
+
+    function generateRandomNum(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function generateCrystalNum() {
+        var crystalNum = generateRandomNum(1, 12);
+
+        do {
+            crystalNum = generateRandomNum(1, 12);
+        } while (crystalValues.indexOf(crystalNum) !== -1);
+
+        crystalValues.push(crystalNum);
+
+        return crystalNum;
+    }
+
+    function generateGameNum() {
+        return generateRandomNum(19, 120);
+    }
+
+    function updateWins() {
+        wins += 1;
+
+        winsCountDOM.html(wins);
+        setupGame();
+    }
+
+    function updateLosses() {
+        losses += 1;
+
+        lossesCountDOM.html(losses);
+        setupGame();
+    }
+
+    function updateScore(num) {
+        var toNum = Number(num);
+        var type = typeof toNum;
+        score += Number(num);
+        console.log(score);
+        scoreDOM.html(score.toString());
+
+        if (randomNum === score) {
+            updateWins();
+        } else if (randomNum < score) {
+            updateLosses();
+        } else {}
+    }
+
+    init();
+
+});
